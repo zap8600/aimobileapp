e4e328def799400430c99b52bd224f94c8c6f472
zap8600 <95487746+zap8600@users.noreply.github.com>
2023-03-30 18:40:43 -0500

    holy shit its working

--- app/src/main/java/io/github/zap8600/aimobileapp/MainActivity.kt
+++ app/src/main/java/io/github/zap8600/aimobileapp/MainActivity.kt
@@ -1,56 +1,22 @@
 package io.github.zap8600.aimobileapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.JsonReader
-import android.widget.Button
-import android.widget.EditText
-import io.github.zap8600.aimobileapp.tokenization.GPT2Tokenizer
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import org.tensorflow.lite.Interpreter
+import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import io.github.zap8600.aimobileapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var tokenizer: GPT2Tokenizer
-    private lateinit var tflite: Interpreter
+    private val gpt2: io.github.zap8600.aimobileapp.ml.GPT2Client by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val userPrompt = findViewById<EditText>(R.id.userPrompt)
-        val generate = findViewById<Button>(R.id.generate)
+        val binding: ActivityMainBinding
+                = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        //Initialize tokenizer
-        tokenizer = loadTokenizer()
-    }
+        binding.vm = gpt2
 
-    private fun loadTokenizer(): GPT2Tokenizer {
-        //Create encoder of Map<String, Int>
-        val encoder = mutableMapOf<String, Int>()
-        val vocabStream = application.assets.open("vocab.json")
-        vocabStream.use {
-            val vocabReader = JsonReader(InputStreamReader(it, "UTF-8"))
-            vocabReader.beginObject()
-            while (vocabReader.hasNext()) {
-                val key = vocabReader.nextName()
-                val value = vocabReader.nextInt()
-                encoder[key] = value
-            }
-            vocabReader.close()
-        }
-        val decoder  = encoder.entries.associateBy({ it.value }, { it.key })
-        val bpeRanks = mutableMapOf<Pair<String, String>, Int>()
-        val mergesStream = application.assets.open("merges.txt")
-        mergesStream.use { stream ->
-            val mergesReader = BufferedReader(InputStreamReader(stream))
-            mergesReader.useLines { seq ->
-                seq.drop(1).forEachIndexed { i, s ->
-                    val list = s.split(" ")
-                    val keyTuple = list[0] to list[1]
-                    bpeRanks[keyTuple] = i
-                }
-            }
-        }
-        return GPT2Tokenizer(encoder, decoder, bpeRanks)
+        binding.lifecycleOwner = this
     }
 }
